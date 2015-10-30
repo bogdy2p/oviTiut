a0eaf8997e3961401d6f3587855aff7a3bb61966
Popa Bogdan Constantin <bogdan.popa@reea.net>
2015-10-30 23:06:01 +0200

    Created reception entity

---
+++ Backend/src/MissionControl/Bundle/OviappBundle/Entity/Reception.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace MissionControl\Bundle\OviappBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reception
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Reception
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="client", type="string", length=255)
+     */
+    private $client;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="date_created", type="string", length=255)
+     */
+    private $dateCreated;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="date_updated", type="string", length=255)
+     */
+    private $dateUpdated;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="creator", type="string", length=255)
+     */
+    private $creator;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="products", type="string", length=255)
+     */
+    private $products;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set client
+     *
+     * @param string $client
+     *
+     * @return Reception
+     */
+    public function setClient($client)
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    /**
+     * Get client
+     *
+     * @return string
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * Set dateCreated
+     *
+     * @param string $dateCreated
+     *
+     * @return Reception
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreated
+     *
+     * @return string
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * Set dateUpdated
+     *
+     * @param string $dateUpdated
+     *
+     * @return Reception
+     */
+    public function setDateUpdated($dateUpdated)
+    {
+        $this->dateUpdated = $dateUpdated;
+
+        return $this;
+    }
+
+    /**
+     * Get dateUpdated
+     *
+     * @return string
+     */
+    public function getDateUpdated()
+    {
+        return $this->dateUpdated;
+    }
+
+    /**
+     * Set creator
+     *
+     * @param string $creator
+     *
+     * @return Reception
+     */
+    public function setCreator($creator)
+    {
+        $this->creator = $creator;
+
+        return $this;
+    }
+
+    /**
+     * Get creator
+     *
+     * @return string
+     */
+    public function getCreator()
+    {
+        return $this->creator;
+    }
+
+    /**
+     * Set products
+     *
+     * @param string $products
+     *
+     * @return Reception
+     */
+    public function setProducts($products)
+    {
+        $this->products = $products;
+
+        return $this;
+    }
+
+    /**
+     * Get products
+     *
+     * @return string
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+}
+
